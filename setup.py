d1dd00dfa751201e905394b02828b0f9cbc9c7c8
kkmore23 <kaustubh23more@gmail.com>
2024-03-16 09:58:59 -0500

    file update

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Kaustubh More',
     author_email='kaustubh.mor@gmil.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","pandas"],
     packages=find_packages()
 )
